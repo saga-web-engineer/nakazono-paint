7f7d80a100dcb699719ff8f7fa37fe41cff46bb2
kumeyuta <ku8.m.1.e2@gmail.com>
2023-09-03 15:11:58 +0900

    バナー部分の修正

--- src/ts/fixBanner.ts
+++ src/ts/fixBanner.ts
@@ -6,13 +6,12 @@ const fixBanner = () => {
   const banner = document.querySelector<HTMLAnchorElement>('#fix-banner');
 
   if (200 < window.scrollY && window.matchMedia('(min-width: 1880px)').matches) {
-    banner?.setAttribute("aria-hidden", "false");
+    banner?.setAttribute('aria-hidden', 'false');
   } else {
-    banner?.setAttribute("aria-hidden", "true");
+    banner?.setAttribute('aria-hidden', 'true');
   }
 };
 
 addEventListener('scroll', fixBanner, false);
 
-export { };
-
+export {};
